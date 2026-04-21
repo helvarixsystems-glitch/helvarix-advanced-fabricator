93740c02df67206fe3c51a03a2ed40871dbfb011
helvarixsystems-glitch <helvarixsystems@gmail.com>
2026-04-21 13:50:42 -0400

    Update index.ts

--- packages/pricing/src/index.ts
+++ packages/pricing/src/index.ts
@@ -1,1 +1,5 @@
+import { componentRegistry } from "@haf/component-registry";
 
+export function estimateGenerationTokens(partFamily: string): number {
+  return componentRegistry.find((item) => item.key === partFamily)?.baseTokenCost ?? 10;
+}
