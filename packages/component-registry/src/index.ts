43c6ee3731f2cf9cf78bfacb1f0a4837c057baf0
helvarixsystems-glitch <helvarixsystems@gmail.com>
2026-04-21 13:50:03 -0400

    Update index.ts

--- packages/component-registry/src/index.ts
+++ packages/component-registry/src/index.ts
@@ -1,1 +1,22 @@
-
+export const componentRegistry = [
+  {
+    key: "nosecone",
+    label: "Nosecone",
+    baseTokenCost: 12
+  },
+  {
+    key: "shell",
+    label: "Structural Shell",
+    baseTokenCost: 14
+  },
+  {
+    key: "rover-arm",
+    label: "Rover Arm",
+    baseTokenCost: 18
+  },
+  {
+    key: "grid-fin",
+    label: "Grid Fin",
+    baseTokenCost: 16
+  }
+] as const;
