fd34db74b3a033f920b9691d4e69ea42bc827772
helvarixsystems-glitch <helvarixsystems@gmail.com>
2026-04-24 09:32:45 -0400

    Create index.ts

---
+++ services/generation-engine/src/simulation/adapters/index.ts
@@ -0,0 +1,10 @@
+export {
+  CalculixAdapter,
+  type CalculixAdapterOptions,
+  type CalculixInputBundle,
+} from "./calculixAdapter";
+
+export {
+  GmshAdapter,
+  type GmshGeometryBundle,
+} from "./gmshAdapter";
